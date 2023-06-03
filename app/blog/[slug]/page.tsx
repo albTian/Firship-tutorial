f7a10020e0797d81b7e17c6682e42d5ec099cf86
Albert Tian <atian@berkeley.edu>
2023-06-02 22:55:19 -0400

    content: no env, no fetching

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -14,37 +14,37 @@ interface Props {
 
 // To use dynamic metadata
 const generateMetadata = async ({params}: Props): Promise<Metadata> => {
-  const posts: Post[] = await fetch(process.env.NEXTAUTH_URL!).then(
-    (res) => res.json()
-  );
-  const post = posts.find((post) => post.slug === params.slug)!;
+  // const posts: Post[] = await fetch(process.env.NEXTAUTH_URL!).then(
+  //   (res) => res.json()
+  // );
+  // const post = posts.find((post) => post.slug === params.slug)!;
   return {
-    title: post.title
+    title: "post.title"
   }
 }
 
 // To use static generation, for dynamic content that doesn't change often
-const generateStaticParams = async () => {
-  const posts: Post[] = await fetch(process.env.NEXTAUTH_URL!).then(
-    (res) => res.json()
-  );
-  return posts.map((post) => ({
-    slug: post.slug
-  }))
-};
+// const generateStaticParams = async () => {
+//   const posts: Post[] = await fetch(process.env.NEXTAUTH_URL!).then(
+//     (res) => res.json()
+//   );
+//   return posts.map((post) => ({
+//     slug: post.slug
+//   }))
+// };
 
 const BlogPostPage = async ({ params }: Props) => {
-  const posts: Post[] = await fetch(process.env.NEXTAUTH_URL!).then(
-    (res) => res.json()
-  );
-  const post = posts.find((post) => post.slug === params.slug)!;
+  // const posts: Post[] = await fetch(process.env.NEXTAUTH_URL!).then(
+  //   (res) => res.json()
+  // );
+  // const post = posts.find((post) => post.slug === params.slug)!;
   return (
     <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
+      <h1>{'post.title'}</h1>
+      <p>{'post.content'}</p>
     </div>
   );
 };
 
 export default BlogPostPage;
-export { generateStaticParams, generateMetadata };
+export { generateMetadata };
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 import { Post } from "./[slug]/page";
 
 const BlogPage = async () => {
-  const posts: Post[] = await fetch(process.env.NEXTAUTH_URL!).then(
-    (res) => res.json()
-  );
+  // const posts: Post[] = await fetch(process.env.NEXTAUTH_URL!).then(
+  //   (res) => res.json()
+  // );
+  const posts: Post[] = []
   return (
     <div>
         <ul>
