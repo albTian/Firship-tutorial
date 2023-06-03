9c4b5d5be11fb07b2a68900c3e802d7d9c0dd510
Albert Tian <atian@berkeley.edu>
2023-06-02 23:38:42 -0400

    misc: no follow button:

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -37,8 +37,8 @@ export default async function UserProfile({ params }: Props) {
       <h3 className="text-2xl font-semibold text-gray-700">Bio</h3>
       <p className="mt-2 text-gray-600">{bio}</p>
 
-      {/* @ts-expect-error Server Component */}
-      <FollowButton targetUserId={id || ""} />
+      {/* unused @ts-expect-error Server Component */}
+      {/* <FollowButton targetUserId={id || ""} /> */}
     </div>
   );
 }
